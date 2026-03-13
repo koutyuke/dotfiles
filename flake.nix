ead4a5d74730652b398ed03cbaa105b2507c9574
koutyuke <75959529+koutyuke@users.noreply.github.com>
2026-03-13 16:58:34 +0900

    feat: add brew-api and brew-nix modules to flake configuration for improved package management

--- flake.nix
+++ flake.nix
@@ -17,6 +17,18 @@
       url = "github:numtide/treefmt-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    brew-nix = {
+      url = "github:BatteredBunny/brew-nix";
+      inputs = {
+        brew-api.follows = "brew-api";
+        nix-darwin.follows = "nix-darwin";
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+    brew-api = {
+      url = "github:BatteredBunny/brew-api";
+      flake = false;
+    };
   };
 
   outputs =
