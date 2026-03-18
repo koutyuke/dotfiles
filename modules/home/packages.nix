7c402322d143f92332e8a9bae2eb0e2ec1baff50
koutyuke <75959529+koutyuke@users.noreply.github.com>
2026-03-18 16:25:41 +0900

    refactor: consolidate Homebrew casks into packages.nix for improved organization and maintainability

--- modules/home/packages.nix
+++ modules/home/packages.nix
@@ -1,27 +1,56 @@
 { pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    # CLI tools
-    act
-    bun
-    ffmpeg
-    fd
-    ghq
-    httpie
-    jq
-    lefthook
-    neofetch
-    ni
-    nodejs_24
-    openssl
-    ripgrep
-    tree
-    watchman
-    yq-go
+  home.packages =
+    with pkgs;
+    [
+      # CLI tools
+      act
+      bun
+      ffmpeg
+      fd
+      ghq
+      httpie
+      jq
+      lefthook
+      neofetch
+      ni
+      nodejs_24
+      openssl
+      ripgrep
+      tree
+      watchman
+      yq-go
 
-    # Development tools
-    cocoapods
-    mise
-    uv
-  ];
+      # Development tools
+      cocoapods
+      mise
+      uv
+    ]
+    ++ (with pkgs.brewCasks; [
+      # appcleaner
+      # arc
+      # azookey
+      # canva
+      # chatgpt
+      clipy
+      # cursor
+      # dbvisualizer
+      # devtoys
+      # figma
+      # ghostty
+      # jordanbaird-ice
+      # karabiner-elements
+      # keyboardcleantool
+      # monitorcontrol
+      # mos
+      # notion
+      # notunes
+      # orbstack
+      # postman
+      # raycast
+      # spotify
+      # the-unarchiver
+      # visual-studio-code
+      # zoom
+    ]);
 }
