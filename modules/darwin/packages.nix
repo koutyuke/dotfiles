2a6b52b37a99ddbe11df03550f3cbc7059eca453
koutyuke <75959529+koutyuke@users.noreply.github.com>
2026-03-19 05:04:26 +0900

    feat: expand systemPackages in packages.nix to include additional Homebrew casks for enhanced macOS application management

--- modules/darwin/packages.nix
+++ modules/darwin/packages.nix
@@ -1,9 +1,19 @@
 { pkgs, ... }:
 {
-  environment.systemPackages = with pkgs; [
-    vim
-    curl
-    git
-    wget
-  ];
+  environment.systemPackages =
+    with pkgs;
+    [
+      vim
+      curl
+      git
+      wget
+    ]
+    ++ (with pkgs.brewCasks; [
+      appcleaner
+      arc
+      raycast
+      monitorcontrol
+      mos
+      zoom
+    ]);
 }
