d22e86963bd29fb53e634763dc5ef8303e8f9e3b
koutyuke <75959529+koutyuke@users.noreply.github.com>
2026-03-29 10:41:22 +0900

    feat: enhance zsh aliases with new commands for improved navigation and Docker management

--- nix/modules/home/programs/zsh/alias.nix
+++ nix/modules/home/programs/zsh/alias.nix
@@ -2,19 +2,31 @@
 {
   programs.zsh = {
     shellAliases = {
-      proot = "cd $(git rev-parse --show-toplevel)";
-
-      # ls = "eza --icons=auto";
     };
 
     zsh-abbr = {
       enable = true;
       abbreviations = {
-        l = "eza --icons=auto --group-directories-first -h";
-        la = "l -a";
-        ll = "l -l -a";
-        lt = "l --tree";
+        cdr = "cd $(git rev-parse --show-toplevel)";
+        l = "eza --icons=auto --group-directories-first";
+        la = "eza --icons=auto --group-directories-first -a";
+        ll = "eza --icons=auto --group-directories-first -hla";
+        lt = "eza --icons=auto --group-directories-first -aTL1";
+        o = "open";
+
+        # docker compose
+        dc = "docker compose";
+        dcu = "docker compose up";
+        dcd = "docker compose down";
+
+        # nix aliases
+        ns = "nix-shell";
+        ngc = "nix-collect-garbage";
       };
     };
+
+    initContent = ''
+      unalias l la ll lt 2>/dev/null
+    '';
   };
 }
